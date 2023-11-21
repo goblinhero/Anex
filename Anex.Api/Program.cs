68e3e30a02498b4508b264c073842deb9d08f7af
Magnus Toftdal <toftdal@hey.com>
2023-11-21 08:15:52 +0100

    DB-changes to postgres

--- Anex.Api/Program.cs
+++ Anex.Api/Program.cs
@@ -1,11 +1,17 @@
 using Anex.Api.Database;
 using Anex.Api.Database.Mappings;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-SessionHelper.Initialize(false, @"Server=.\SQLEXPRESS;initial catalog=Testers;Integrated Security=true", typeof(LedgerTagMapping));
+var connectionString = builder.Configuration.GetConnectionString("POSTGRESQLCONNSTR_CONNECTION_ANEX_DB");
+
+if (!string.IsNullOrEmpty(connectionString))
+{
+    SessionHelper.Initialize(true, connectionString, typeof(LedgerTagMapping));
+}
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
